f2bf372584faf66e6418feddd1141e8bfe93a8f5
Nana-OT <83919938+Nana-OT@users.noreply.github.com>
2021-11-03 22:26:42 -1200

    updated

--- screens/Details.js
+++ screens/Details.js
@@ -4,21 +4,59 @@ import { StyleSheet, Text, View,ImageBackground,TouchableOpacity, Image } from '
 const Details = ({ navigation }) => {
     return (
         <View>
+            <View>
+                <Text style={styles.txt}> Products Available</Text>
+            </View>
+            <View>
             <TouchableOpacity>
-            <Image source = {require('../assets/bike1.png')} style={StyleSheet.bike1}/>
+            <Image source = {require('../assets/bike1.png')} style={styles.bike1}/>
             </TouchableOpacity>
+            </View>
+            <View>
+            <TouchableOpacity >
+            <Image source = {require('../assets/bike2.png')} style={styles.bike2}/>
+            </TouchableOpacity>
+            </View>
+            <View style={styles.view3}>
+            <TouchableOpacity>
+            <Image source = {require('../assets/bike3.png')} style={styles.bike3}/>
+            </TouchableOpacity>
+            </View>
+      
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     bike1:{
-        width: 50,
-        height: 50,
+        width: "50%",
+        height: "50%",
+        padding: 20,
+        marginTop: 40,
+        resizeMode: "contain",
+    },
+    bike2:{
+        width: "50%",
+        height: "65%",
+        bottom: 750,
+        marginHorizontal: 200,
+        resizeMode: "contain",
+
+    },
+    bike3:{
         
     },
-});
+    view3:{
+        resizeMode: "contain",
 
+    },
+    txt:{
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "500"
+
+    },
+});
 
 
 export default Details;
